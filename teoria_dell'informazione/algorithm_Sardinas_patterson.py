3e873573346a69229f160ad2ae250599c529e222
SalvatoreAntoninoGristina <gristinasalvatore14@gmail.com>
2020-03-19 21:02:22 +0100

    final

--- teoria_dell'informazione/algorithm_Sardinas_patterson.py
+++ teoria_dell'informazione/algorithm_Sardinas_patterson.py
@@ -1,29 +1,89 @@
+"""
+Salvatore Antonino Gristina
+abc abcd e dba bace ceac ceab eabd
+a c ad abb bad deb bbcde
+00 01 10 11
+0 10 110 1110
+"""
 
 
 def init():
-    return input("Inserire il codice separado da uno spazio \n").lower().split(",")
+    return input("Inserire il codice separado ogni elemento da uno spazio \n").split(" ")
 
 
 def run():
-    table.append(s0)
 
-    createS1(table[0])
+    table.append(s0)    # inserisco la colonna all'interno della tabella
 
+    i = create_and_check_S1(table[0])    # creo S1
 
-def createS1(table1):
+    while i != 0:
+        i = create_and_check_Sn(table[0], table[i], i)    # creo colonne finchè non arrivo ad una condizione di arresto
+
+    print_table()
+
+def create_and_check_S1(table1):
     tmp = list()
-    for elm1 in table1:
-        for elm2 in table1:
-            if elm1 != elm2:
-                if elm1.find(elm2) == 0:
-                    tmp.append(elm1[len(elm2):])
+    for elem1 in table1:
+        for elem2 in table1:
+            if elem1 != elem2:
+                if elem1.find(elem2) == 0:
+                    tmp.append(elem1[len(elem2):])
 
-    table.append(list(set(tmp)))
-    print(table[0])
+    if len(tmp) == 0:
+        print("Il codice è UD poiché S1 è vuoto")
+        return 0
+    else:
+        table.append(list(set(tmp)))
 
+    return 1
+
+
+def create_and_check_Sn(S0, Si, i):
+    tmp = list()
+    for elem1 in S0:
+        for elem2 in Si:
+            if elem1 != elem2:
+                if elem1.find(elem2) == 0:
+                    tmp.append(elem1[len(elem2):])
+                else:
+                    if elem2.find(elem1) == 0:
+                        tmp.append(elem2[len(elem1):])
+    if len(tmp) == 0:
+        print("Il codice è UD poiché S{} è vuoto." .format(i+1))
+        return 0
+    else:
+        table.append(list(set(tmp)))
+
+        set0 = set(table[0])
+        seti = set(table[i + 1])
+
+        for j in range(len(table) - 1):
+            setj = set(table[j])
+            if setj == seti:
+                print(
+                    "Il codice è UD poichè l'insieme S{} è uguale all'insieme S{}".format(i + 1, j))
+                print(table[1])
+                return 0
+
+        if len(set0.intersection(seti)) != 0:
+            print("Il codice non è UD perchè "+
+                  "l'insieme S0 e l'insieme S{} hanno elementi in comune".format(i + 1))
+            return 0
+
+    return i + 1
+
+
+def print_table():
+
+        for i in range(0, len(table)):
+            print("S{}: {} ".format(i, table[i]))
+
+
+
+
+# main dove creo una tabella e la prima colonna S0
 if __name__ == '__main__':
     s0 = list(init())
     table = list()
     run()
-    # abc abcd e dba bace ceac ceab eadb
-    # 0 10 110 1110
